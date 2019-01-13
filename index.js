89b6132433cf56bb7beb514ce201583431993f6e
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-13 14:35:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var name = "Garrett"
 var height = "74"
+var height1 = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)-2)
+console.log(parseInt(height)-2) //parseInt()
+console.log(height1 - 2) //int
